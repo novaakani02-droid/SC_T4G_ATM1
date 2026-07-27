ce082324beb3cd11bcf1ec1309ea7820055ea4d8
novaakani02-droid <novaakani02@gmail.comgit config --global user.name>
2026-07-27 12:57:39 +0000

    linked external js to my work

--- src/atm_prompt.js
+++ src/atm_prompt.js
@@ -5,12 +5,28 @@ const receiptTitle = document.getElementById("receipt-title");
 const receiptText = document.getElementById("receipt-text");
 
 // 1. SET THEINITIAL BALANCE & SECURITY DATA
-
+let userBalance = "1000";
+let pin ="1468";
 
 // 2. ASK THE USER FOR INPUT (Runs automatically as soon as the page loads)
-
+let enteredpin= prompt("Enter your 4-digit pin");
 
 // 3. VALIDATE / CHECK THE PIN NUMBER
+if (enteredpin === pin){
+   let userAmount = prompt(`PIN ACCEPTED! Your current balance is ${userBalance}, Enter amount to withdraw`);
+    console.log(typeof(userAmount));//string
+    userAmount= Number(userAmount)
+    console.log(typeof(userAmount)); //number
+    if (userBalance >= userAmount){
+        let newUserBalance = userBalance - userAmount;
+        // userBalance = userBalance - userBcalance
+        // userBalance -= userAmount;
+
+        receiptText.innerText = "TRANSACTION SUCESSFUL";
+    }
+ } else {
+prompt(`INCORRECT PIN! Try again`);
+ }
 
     
     
